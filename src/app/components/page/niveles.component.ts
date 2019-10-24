fa351575140e949885ac13fc9a442e54fe46218c
JohnWarriorCor <guerrerocordobajohn@gmail.com>
2019-10-24 17:27:46 -0500

    Modulo niveles 24/10/2019 5:27 pm

--- src/app/components/page/niveles.component.ts
+++ src/app/components/page/niveles.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./niveles.component.css']
 })
 export class NivelesComponent implements OnInit {
-
+  hover1 = false;
+  hover2 = false;
+  hover3 = false;
+  hover4 = false;
+  hover5 = false;
+  hover6 = false;
+  hover7 = false;
+  hover8 = false;
+  hover9 = false;
   constructor() { }
 
   ngOnInit() {
