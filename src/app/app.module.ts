d27c3d9d5aec1229107ed5cea5730dcb3ba61ee8
JohnWarriorCor <guerrerocordobajohn@gmail.com>
2019-10-24 15:48:06 -0500

    Modulo niveles 24/10/2019 3:47 pm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { APP_ROUTING } from './app.routing';
 import { PortadaComponent } from './components/portada.component';
 import { Resta05Component } from './components/page/resta05/resta05.component';
 import { Suma05Component } from './components/page/suma05.component';
+import { NivelesComponent } from './components/page/niveles.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { Suma05Component } from './components/page/suma05.component';
     HomeComponent,
     PortadaComponent,
     Resta05Component,
-    Suma05Component
+    Suma05Component,
+    NivelesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './components/home.component';
 import { PortadaComponent } from './components/portada.component';
 import { Resta05Component } from './components/page/resta05/resta05.component';
 import { Suma05Component } from './components/page/suma05.component';
+import { NivelesComponent } from './components/page/niveles.component';
 
 const routes: Routes = [
   { path: 'portada', component: PortadaComponent },
   { path: 'home', component: HomeComponent },
   { path: 'suma', component: Suma05Component },
   { path: 'resta', component: Resta05Component },
+  { path: 'niveles', component: NivelesComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'portada' }
 ];
 
