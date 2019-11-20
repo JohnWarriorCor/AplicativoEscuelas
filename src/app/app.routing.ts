a4f6706b0becd5ae724cbb584fb788a4135caee7
JohnWarriorCor <guerrerocordobajohn@gmail.com>
2019-11-20 18:03:48 -0500

    Componente suma20

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,9 +4,11 @@ import { PortadaComponent } from './components/portada.component';
 import { Resta05Component } from './components/page/resta05/resta05.component';
 import { Suma05Component } from './components/page/suma05/suma05.component';
 import { NivelesComponent } from './components/page/niveles/niveles.component';
+import { Suma20Component } from './components/page/suma20/suma20.component';
 
 const routes: Routes = [
   { path: 'portada', component: PortadaComponent },
+  { path: 'suma3', component: Suma20Component },
   { path: 'home', component: HomeComponent },
   { path: 'suma', component: Suma05Component },
   { path: 'resta', component: Resta05Component },
