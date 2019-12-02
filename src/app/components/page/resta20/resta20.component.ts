9032c6b9cd3c0325f6c97bd79ab52849babfb438
JohnWarriorCor <guerrerocordobajohn@gmail.com>
2019-12-02 15:46:57 -0500

    Componente resta nuevo

---
+++ src/app/components/page/resta20/resta20.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resta20',
+  templateUrl: './resta20.component.html',
+  styleUrls: ['./resta20.component.css']
+})
+export class Resta20Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
